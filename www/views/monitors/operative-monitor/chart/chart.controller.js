84f0c6b75d1fa3966690ddc224bc051368b85428
maxenz <maximilianopoggio@gmail.com>
2016-07-05 10:06:28 -0300

    Cambio de color tab de graphs

--- www/views/monitors/operative-monitor/chart/chart.controller.js
+++ www/views/monitors/operative-monitor/chart/chart.controller.js
@@ -25,26 +25,15 @@
 
     function activate() {
 
-
         vm.graph = {};
         vm.graph.labels = operativeMonitorService.chartLabels;
         vm.graph.series = ['Conformidad %'];
         vm.graph.data = [operativeMonitorService.chartData];
-        //vm.graph.options = {responsive: false};
-        console.log($scope.graph);
-
-
-      // $scope.graph = {};                        // Empty graph object to hold the details for this graph
-      // $scope.graph.data = [                     // Add bar data, this will set your bars height in the graph
-      //   //Awake
-      //   [16, 15, 20, 12, 16, 12, 8],
-      //   //Asleep
-      //   [8, 9, 4, 12, 8, 12, 14]
-      // ];
-      // $scope.graph.labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];    // Add labels for the X-axis
-      // $scope.graph.series = ['Awake', 'Asleep'];  // Add information for the hover/touch effect
-
-
+        vm.graph.colours =  [{
+          "fillColor": "#e74c3c",
+          "strokeColor": "#e74c3c",
+          "pointColor": "#e74c3c"
+        }];
 
     }
 
